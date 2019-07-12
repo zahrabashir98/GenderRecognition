051552e91922e79f0e0884281e56fde3413e2c13
ghazalb76 <ghazal.b93@gmail.com>
2019-07-13 00:21:56 +0430

    Add total accuracy

--- src.py
+++ src.py
@@ -4,26 +4,33 @@ import librosa, librosa.display
 import glob
 from scipy.io import wavfile
 import numpy as np
+import scipy
+from scipy.io.wavfile import read
+from scipy.signal import hann
+from scipy.fftpack import rfft
 
 
 counter = 0
-koll = 0
+total = 0
 num_of_female_f = 0
 num_of_male_f = 0
 no_detection_f = 0
-path = "data/female/*.wav"
+path = "data/all/*.wav"
 flag = True
+correct_detection = 0
 file_name_list = []
 for filename in glob.glob(path):
-    # print(filename)
-    koll += 1
+    total += 1
     x, sr = librosa.load(filename)
     X = scipy.fft(x)
     X_mag = numpy.absolute(X)        # spectral magnitude
+
     if(max(X_mag)>113.32542591236971 or max(X_mag)< 20.865958116020522):
         print('mardddddd')
         flag = False
-        num_of_male_f += 1
+        if(filename[9] == 'm'):
+            correct_detection += 1
+            num_of_male_f += 1
     else:
         fs, signal = wavfile.read(filename)
         signal = signal / max(abs(signal))                        # scale signal
@@ -48,10 +55,21 @@ for filename in glob.glob(path):
         if max(STEs) <12.5 and max(STEs)>5:
             print("male")
             flag = False
+            if(filename[9] == 'm'):
+                num_of_male_f += 1
+                correct_detection += 1
+        elif (np.mean(STEs) < 0.7149665232994251 ):
+            print("male")
+            flag = False
             num_of_male_f += 1
+            if(filename[9] == 'm'):
+                correct_detection += 1
+                num_of_male_f += 1
     if(flag):
         print('female')
         num_of_female_f += 1
-print(num_of_female_f/koll)
-print(koll)
-        
+        if(filename[9] == 'f'):
+            correct_detection += 1
+            num_of_female_f += 1
+
+print('accuracy : ', correct_detection/total)
